29376de9006333e2524f5f055b9fe8bfea249ce1
chirravenkatkumar <venkatkumarchirra@gmail.com>
2024-06-19 11:50:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+// This script is currently empty because the basic functionality is handled by CSS
+// You can add JavaScript here if you need additional features, like a dynamic menu
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('JavaScript Loaded');
+    // Add any JavaScript here if needed
+});
